2a82c25e765a416d136e25ea9a50a9e53e18b1eb
Nam Ta Van <gosei_namtv@outlook.com>
2019-12-26 16:09:27 +0700

    update -db

---
+++ GoseiVn.DemoApp/4.8.0/aspnet-core/src/GoseiVn.DemoApp.EntityFrameworkCore/Migrations/20191226090806_Alter_ColumnType_Estimate.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GoseiVn.DemoApp.Migrations
+{
+    public partial class Alter_ColumnType_Estimate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ImportantNote",
+                table: "Estimates",
+                type: "NVARCHAR(500)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "NVARCHAR(100)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Estimates",
+                type: "NVARCHAR(50)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "NVARCHAR(25)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "ImportantNote",
+                table: "Estimates",
+                type: "NVARCHAR(100)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "NVARCHAR(500)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Estimates",
+                type: "NVARCHAR(25)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "NVARCHAR(50)",
+                oldNullable: true);
+        }
+    }
+}
